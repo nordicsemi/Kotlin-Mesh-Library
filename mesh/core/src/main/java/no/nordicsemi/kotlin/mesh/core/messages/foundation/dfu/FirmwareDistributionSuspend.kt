6dd8014d5f921d86f692ebd4d5d73bb426cfb2f5
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-03-31 20:57:45 +0200

    Adds support for FirmwareDistributionSuspend

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/dfu/FirmwareDistributionSuspend.kt
@@ -0,0 +1,22 @@
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.dfu
+
+import no.nordicsemi.kotlin.mesh.core.messages.AcknowledgedMeshMessage
+import no.nordicsemi.kotlin.mesh.core.messages.FirmwareDistributionMessageInitializer
+
+/**
+ * Firmware Distribution Get message is an acknowledged message sent by a Firmware Distribution
+ * Client to get the state of the firmware image distribution on a Firmware Distribution Server.
+ */
+class FirmwareDistributionSuspend : AcknowledgedMeshMessage {
+    override val opCode: UInt = Initializer.opCode
+    override val responseOpCode = FirmwareDistributionStatus.opCode
+    override val parameters = null
+
+    companion object Initializer : FirmwareDistributionMessageInitializer {
+        override val opCode = 0x831Au
+
+        override fun init(parameters: ByteArray?) = parameters
+            ?.takeIf { it.isEmpty() }
+            ?.let { FirmwareDistributionSuspend() }
+    }
+}
