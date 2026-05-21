4bf698b071b2a752e61ac9de00cd8c938f75b662
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-05-21 10:00:35 +0200

    Updates models that supports publications

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Model.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Model.kt
@@ -334,6 +334,16 @@ class Model internal constructor(
             LIGHT_LC_SETUP_SERVER_MODEL_ID,
             LIGHT_LC_CLIENT_MODEL_ID,
                 -> true
+            // BLOB Transfer
+            BLOB_TRANSFER_SERVER_MODEL_ID,
+            BLOB_TRANSFER_CLIENT_MODEL_ID,
+                -> false
+            // Device Firmware Update
+            FIRMWARE_UPDATE_SERVER_MODEL_ID,
+            FIRMWARE_UPDATE_CLIENT_MODEL_ID,
+            FIRMWARE_DISTRIBUTION_SERVER_MODEL_ID,
+            FIRMWARE_DISTRIBUTION_CLIENT_MODEL_ID,
+                -> false
 
             else -> null
         }
