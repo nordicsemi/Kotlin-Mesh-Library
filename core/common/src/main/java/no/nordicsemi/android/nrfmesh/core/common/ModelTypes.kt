cd7ee60529cd81d8e8f7298281bcb59f0ef53ad1
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-06-08 09:12:19 +0200

    Minor refactoring

--- core/common/src/main/java/no/nordicsemi/android/nrfmesh/core/common/ModelTypes.kt
+++ core/common/src/main/java/no/nordicsemi/android/nrfmesh/core/common/ModelTypes.kt
@@ -42,11 +42,8 @@ fun isSupportedGroupItem(model: Model) = model.isGenericOnOffServer() ||
 fun Model.isFirmwareDistributionServer() = isBluetoothSigAssigned
         && (modelId as SigModelId).modelIdentifier == Model.FIRMWARE_DISTRIBUTION_SERVER_MODEL_ID
 
-fun Model.isFirmwareDistributionClient() = isBluetoothSigAssigned
-        && (modelId as SigModelId).modelIdentifier == Model.FIRMWARE_DISTRIBUTION_CLIENT_MODEL_ID
-
 fun Model.isFirmwareUpdateServer() = isBluetoothSigAssigned
         && (modelId as SigModelId).modelIdentifier == Model.FIRMWARE_UPDATE_SERVER_MODEL_ID
 
-fun Model.isFirmwareUpdateClient() = isBluetoothSigAssigned
-        && (modelId as SigModelId).modelIdentifier == Model.FIRMWARE_UPDATE_CLIENT_MODEL_ID
+fun Model.isLePairingResponderServer() =
+    (modelId as VendorModelId).modelIdentifier == VendorModelIds.LE_PAIRING_RESPONDER
