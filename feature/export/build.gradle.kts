86ac578e76e058d16db2504c99ff72ab02ea00e2
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-06-08 09:24:44 +0200

    Adds initial support for LE Pairing Initiator and Responder

--- feature/export/build.gradle.kts
+++ feature/export/build.gradle.kts
@@ -12,20 +12,16 @@ dependencies {
     implementation(nordic.kotlin.data)
     implementation(libs.kotlinx.serialization.json)
 
+    implementation(project(":core:data"))
+    implementation(project(":core:ui"))
+    implementation(project(":core:navigation"))
+    implementation(project(":mesh:core"))
+
     testImplementation(libs.junit4)
     testImplementation(libs.kotlin.junit)
     testImplementation(libs.androidx.test.ext)
     testImplementation(libs.androidx.test.rules)
 
-    androidTestImplementation(libs.junit4)
-    androidTestImplementation(libs.kotlin.junit)
-    androidTestImplementation(libs.androidx.test.ext)
-    androidTestImplementation(libs.androidx.test.rules)
-
-    implementation(project(":core:data"))
-    implementation(project(":core:ui"))
-    implementation(project(":core:navigation"))
-    implementation(project(":mesh:core"))
 
 
 }
