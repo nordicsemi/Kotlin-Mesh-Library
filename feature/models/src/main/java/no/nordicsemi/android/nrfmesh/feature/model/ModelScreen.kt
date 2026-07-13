e1c8905ba445851c888ff15c098a211ac7febd5d
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-07-13 22:39:37 +0200

    Fix merge conflicts

--- feature/models/src/main/java/no/nordicsemi/android/nrfmesh/feature/model/ModelScreen.kt
+++ feature/models/src/main/java/no/nordicsemi/android/nrfmesh/feature/model/ModelScreen.kt
@@ -63,10 +63,10 @@ internal fun ModelScreen(
     resetMessageState: () -> Unit,
     onAddGroupClicked: () -> Unit,
     navigateToGroups: () -> Unit,
-    navigateToFirmwareInformation: (Model, FirmwareInformation) -> Unit,
+    navigateToFirmwareInformation: (String, Model, FirmwareInformation) -> Unit,
     navigateToBindApplicationKeys: (Model) -> Unit,
     onRelatedModelsClicked: (Model) -> Unit,
-    startPairing: () -> Unit,
+    startPairing: (Context) -> Unit,
 ) {
     // When entering this screen the TextFields automatically gets focused causing the keyboard
     // to show up. This is a known issue and the workaround is to make the column focusable to
