ee0f3f30342ece397856e987aa1f295f835b8312
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-08-28 19:41:10 +0200

    Clean up

--- core/ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/MeshSingleLineListItem.kt
+++ core/ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/MeshSingleLineListItem.kt
@@ -2,11 +2,9 @@
 
 package no.nordicsemi.android.nrfmesh.core.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
@@ -15,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
